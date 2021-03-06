79d43b02f59b411ec834d7a3eb41c6c6bc3647dc
PWLIMAVERDE <vorfast@gmail.com>
2021-03-06 17:46:01 -0300

    feature ok

--- lib/retorno_sucesso_ou_erro_package.dart
+++ lib/retorno_sucesso_ou_erro_package.dart
@@ -3,6 +3,8 @@ library retorno_sucesso_ou_erro_package;
 export 'src/usecase.dart';
 export 'src/repositorio.dart';
 export 'src/datasource.dart';
+export 'src/features/retorno_bool/presenter/retorno_bool_presenter.dart';
 export 'src/utilitarios/erros.dart';
+export 'src/utilitarios/Parametros.dart';
 export 'src/utilitarios/retorno_sucesso_ou_erro.dart';
 export 'src/utilitarios/tempo_execucao.dart';
